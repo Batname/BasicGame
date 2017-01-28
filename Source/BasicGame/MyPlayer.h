6e99320fbba01f2177c7e39f7e790e7e739429b0
Denis Dubinin <dadubinin@gmail.com>
2017-01-28 10:22:25 +0000

    define player methods

--- Source/BasicGame/MyPlayer.h
+++ Source/BasicGame/MyPlayer.h
@@ -19,31 +19,30 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	/** Move forward player keybord input*/
-	// UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
+	UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
 	void MoveForward(float Val);
 
 	/** Move right player keybord input*/
-	//UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
+	UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
 	void MoveRight(float Val);
 
 	/** Look yaw player mouse input*/
-	//UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
+	UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
 	void LookYaw(float Val);
 
 	/** Look pitch player mouse input*/
-	//UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
+	UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
 	void LookPitch(float Val);
 
 	/** Player use keybord action*/
-	//UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
+	UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
 	void Use();
 
 	/** Forward definition because we cannot include one header file inside onother */
-	//UFUNCTION(BlueprintPure, Category = "MyPlayer")
-	//class AInteractableActor* FindFocusedActor();
+	class AInteractableActor* FindFocusedActor();
 	
 	/** Set action for highliting */
-	//UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
+	UFUNCTION(BlueprintCallable, Category = "MyPlayerInput")
 	void HandleHighLight();
 private:
 
